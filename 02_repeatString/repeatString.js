936d5008abdb6efc799d5a1090170d30e84d578e
Yash7426 <lcs2022057@iiitl.ac.in>
2023-02-12 23:49:09 +0530

    update .js files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let s='';
+    for(let i=0;i<num;i++){
+        s+=str;
+    }
+    return s;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr=str.split('')
+    let arr2=[];
+    let l=arr.length-1;
+    for(let i=l;i>=0;i--){
+        arr2.push(arr[i]);
+    }
+    return arr2.join('');
 };
 
 // Do not edit below this line
